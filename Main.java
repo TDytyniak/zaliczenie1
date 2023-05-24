9d30e1bde7a5a7ea7f94a7c813db56be4dd05c86
TDytyniak <21051422-TDytyniak@users.noreply.replit.com>
2023-05-24 12:36:10 +0000

    1

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj prędkość w m/s: ");
+        double predkoscMS = scanner.nextDouble();
+
+        double predkoscKMH = predkoscMS * 3.6;
+
+        
+        System.out.println("Prędkość w km/h: " + predkoscKMH);
+        
+    
+    }
 }
